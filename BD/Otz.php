ebb6e3891331cb97d50132190d6e86a9bdc964a4
Sergey415 <sergeiperekryostov@yandex.ru>
2020-05-04 18:04:23 +0300

    Небольшие изменения в регистрации и работа над отзывами

--- BD/Otz.php
+++ BD/Otz.php
@@ -3,9 +3,9 @@
     header('Access-Control-Allow-Methods: GET, POST');
     header("Access-Control-Allow-Headers: Content-Type");
     header('Content-Type: application/json;charset=utf-8');
+    $otziv = $_GET["otziv"];
     $mysqli = mysqli_connect("mysql-160578.srv.hoster.ru","srv160578_serega","Sery123","srv160578_serega");
     $mysqli->query("SET NAMES utf8");
-    $otziv = $_GET["otziv"];
-    $sql = "INSERT INTO `Otz` (`id`, `id_user`, `Отзыв`, `was_like`) VALUES (NULL, NULL, '$otziv', 1)";
+    "INSERT INTO `Otz` (`id`, `id_user`, `Отзыв`, `was_like`) VALUES (NULL, '3', '$otziv', '1')";
     $mysqli->query($sql)
 ?>
