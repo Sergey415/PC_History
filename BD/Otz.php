da8fe784e72f029b6317302c2b56e438250f4eaa
Sergey415 <sergeiperekryostov@yandex.ru>
2020-05-18 09:45:27 +0300

    Работа над отзывами + добавил комментарии

--- BD/Otz.php
+++ BD/Otz.php
@@ -6,15 +6,13 @@
     $mysqli = mysqli_connect("mysql-160578.srv.hoster.ru","srv160578_serega","Sery123","srv160578_serega");
     $mysqli->query("SET NAMES utf8");
     $otziv = $_GET["otziv"];
-    $like = $_GET["was_like"];
     $login = $_GET["login"];
-    $pass = $_GET["password"];
-    $id = "SELECT id FROM Logins WHERE login = '$login' AND password = '$pass'";
-   $result = $mysqli->query($id);
-        for($i=0;$row = mysqli_fetch_assoc($result);$i++){
-        $test[] = $row;
-        };
-    $sql = "INSERT INTO `Otz` (`id`, `id_user`, `Отзыв`, `was_like`) VALUES (NULL, '$id', '$otziv', '$like')";
+    $password = $_GET["password"];
+    $id = "SELECT id FROM Logins WHERE login = '$login' AND password = '$password'";
+    $result = $mysqli->query($id);
+    for($i=0;$row = mysqli_fetch_assoc($result);$i++){
+    $test[] = $row;
+    };
+    $sql = "INSERT INTO `Otz` (`id`, `id_user`, `Отзыв`, `was_like`) VALUES (NULL, '$id', '$otziv', '1')";
     $mysqli->query($sql);
-    
 ?>
