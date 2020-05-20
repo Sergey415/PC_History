da886caec0a14f5db2fb2d517ac1b69e18ff28ec
Sergey415 <sergeiperekryostov@yandex.ru>
2020-05-20 20:02:43 +0300

    Допилил игры, сайты...

--- BD/Otz.php
+++ BD/Otz.php
@@ -8,12 +8,13 @@
     $otziv = $_GET["otziv"];
     $login = $_GET["login"];
     $password = $_GET["password"];
+    $like = $_GET["lik"];
     $id = "SELECT id FROM Logins WHERE login = '$login' AND password = '$password'";
     $result = $mysqli->query($id);
     for($i=0;$row = mysqli_fetch_assoc($result);$i++){
     $test[] = $row;
     };
     $id = $test[0]['id'];
-    $sql = "INSERT INTO `Otz` (`id`, `id_user`, `Отзыв`, `was_like`) VALUES (NULL, '$id', '$otziv', '1')";
+    $sql = "INSERT INTO `Otz` (`id`, `id_user`, `Отзыв`, `was_like`) VALUES (NULL, '$id', '$otziv', '$like')";
     $mysqli->query($sql);
 ?>
