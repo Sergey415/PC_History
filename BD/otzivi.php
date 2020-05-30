a1bfd98887b33c4e1266b740ae9ec16cc6ddd4b5
Sergey415 <sergeiperekryostov@yandex.ru>
2020-05-30 08:07:22 +0300

    Работа над отзывами

--- BD/otzivi.php
+++ BD/otzivi.php
@@ -5,11 +5,10 @@
         header('Content-Type: application/json;charset=utf-8');
         $mysqli = mysqli_connect("mysql-160578.srv.hoster.ru","srv160578_serega","Sery123","srv160578_serega");
         $mysqli->query("SET NAMES utf8");
-        $otzivi = "SELECT * FROM `Otz`"
+        $otzivi = "SELECT * FROM `Otz`";
         $result = $mysqli->query($otzivi);
     for($i=0;$row = mysqli_fetch_assoc($result);$i++){
     $test[] = $row;
     };
-    $otzivi = $test[0]['otzivi'];
-    echo($otzivi[0])
+    print json_encode($test, JSON_UNESCAPED_UNICODE);
 ?>
---
+++ js/print_otzivi.js
@@ -0,0 +1,6 @@
+
+  async function await() {
+       let r = await fetch("http://BD/otzivi.php")
+    a = (await r.text())
+  }
+await()
